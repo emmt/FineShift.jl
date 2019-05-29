51b8148eccb0eef1eacc5dc968b8f61f6c353c7d
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2019-05-29 18:02:24 +0200

    Add vectorization (with @simd) and benchmarks

--- src/FineShift.jl
+++ src/FineShift.jl
@@ -36,6 +36,8 @@ dst[i,j,k] = sum_jp src[i,jp,k])*ker(j - jp - t)
 with `ker` the interpolation kernel and for all possible indices.  Indices `i`
 and `k` may be multi-dimensional.
 
+For now, the code only works for *flat* boundary conditions.
+
 See also [`fineshift`](@ref)
 
 """
@@ -112,15 +114,34 @@ getcoefs(ker::Kernel{T}, t::Real) where {T<:AbstractFloat} = getcoefs(ker, T(t))
                 Expr(:return, Expr(:tuple, :l, W...)))
 end
 
-# This worker function assumes that all dimensions have been checked.
 #
-# Typically, on my Intel Core i7-870 at 2.93GHz with S = 4 (e.g. Catmull-Rom
-# spline) and small images, this code achieves 1.26 Gflops with no
+# The generated function `_fineshift!` was tested with Julia 1.1.1 on 2 Intel
+# Core CPUs (i7-870 at 2.93GHz and i7-4790K at 4GHz) with S = 4 (e.g.,
+# Catmull-Rom spline) and small images.  code achieves 1.26 Gflops with no
 # optimizations, 1.75 Gflops with @inbounds.
 #
-# This gives respectively 14.4 µs and 10.4 µs per image of size 36x36.
+# The code is significantly faster (9% to 35%) with @inbounds.  Using @simd on
+# the 2 kinds of loops does not help, on the contrary for the i7-870.  For the
+# two machines, there is a slight improvement with @simd only on the simplest
+# inner loop (when not interpolating the first dimension).
 #
-
+# Below are are the results of the tests with Float64 images of size 36x36 for
+# which 2D interpolation with Catmull-Rom splines requires 36*36*2*7 = 18144
+# floating-point operations.
+#
+#    | Optimizations         | i7-870 @ 2.93GHz        | i7-4790K @ 4.00GHz     |
+#    |:----------------------|:------------------------|:-----------------------|
+#    |                       | 14.966 μs / 1.21 Gflops | 8.357 µs / 2.17 Gflops |
+#    | @inbounds             | 11.080 μs / 1.64 Gflops | 7.709 µs / 2.36 Gflops |
+#    | @inbounds       @simd | 10.400 μs / 1.74 Gflops | 7.313 µs / 2.48 Gflops |
+#    | @inbounds @simd @simd | 11.749 μs / 1.54 Gflops | 7.641 µs / 2.37 Gflops |
+#
+# The first and second @simd respectively indicate whether vectorization is
+# activated for interpolation along the first and/or others dimensions.
+#
+# Using single precision floats (Float32) is slightly faster: 7.210 μs instead
+# of 7.313 µs on the i7-4790K.
+#
 @generated function _fineshift!(dst::AbstractArray{T,N},
                                 src::AbstractArray{T,N},
                                 ker::Kernel{T,S,<:Union{Flat,SafeFlat}},
@@ -130,6 +151,8 @@ end
                                 n::Int,
                                 K::CartesianIndices{Nk},
                                 adj::Bool=false) where {T,S,N,Ni,Nk}
+    # The worker function `_fineshift!` assumes that all dimensions have been
+    # checked so we can use @inbounds.
     @assert S > 1
     W = [Symbol("w",s) for s in 1:S] # all generated weights
     J = [Symbol("j",s) for s in 1:S] # all generated indices
@@ -149,14 +172,35 @@ end
     inds = (:(j0 = j - l),
             [:($(J[s]) = clamp(j0 + $s, 1, n)) for s in 1:S]...)
     if Ni > 0
-        direct = Expr(:for, :(i = I),
-                      Expr(:(=), :(dst[i, j, k]),
-                           Expr(:call, :+,
-                                [:(src[i, $(J[s]), k]*$(W[s])) for s in 1:S]...)))
-        adjoint = Expr(:for, :(i = I),
-                       Expr(:block,
-                            :(tmp = src[i, j, k]),
-                            [:(dst[i, $(J[s]), k] += $(W[s])*tmp) for s in 1:S]...))
+        direct = Expr(:(=), :(dst[i, j, k]),
+                      Expr(:call, :+,
+                           [:(src[i, $(J[s]), k]*$(W[s])) for s in 1:S]...))
+        adjoint = Expr(:block,
+                       :(tmp = src[i, j, k]),
+                       [:(dst[i, $(J[s]), k] += $(W[s])*tmp) for s in 1:S]...)
+        return quote
+            $(coefs...)
+            if adj
+                @inbounds for k in K
+                    for j in 1:m
+                        $(inds...)
+                        @simd for i in I
+                            $adjoint
+                        end
+                    end
+                end
+            else
+                @inbounds for k in K
+                    for j in 1:m
+                        $(inds...)
+                        @simd for i in I
+                            $direct
+                        end
+                    end
+                end
+            end
+            return dst
+        end
     else
         direct = Expr(:(=), :(dst[j, k]),
                       Expr(:call, :+,
@@ -164,27 +208,27 @@ end
         adjoint = Expr(:block,
                        :(tmp = src[j, k]),
                        [:(dst[$(J[s]), k] += $(W[s])*tmp) for s in 1:S]...)
+        return quote
+            $(coefs...)
+            if adj
+                @inbounds for k in K
+                    for j in 1:m
+                        $(inds...)
+                        $adjoint
+                    end
+                end
+            else
+                @inbounds for k in K
+                    for j in 1:m
+                        $(inds...)
+                        $direct
+                    end
+                end
+            end
+            return dst
+        end
     end
 
-    quote
-        $(coefs...)
-        if adj
-            @inbounds for k in K
-                for j in 1:m
-                    $(inds...)
-                    $adjoint
-                end
-            end
-        else
-            @inbounds for k in K
-                for j in 1:m
-                    $(inds...)
-                    $direct
-                end
-            end
-        end
-        return dst
-    end
 end
 
 end # module
